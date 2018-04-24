d4c1662343ef24f3b371cecee68bcc620c0b2fd5
Augusto Boranga <guto.boranga@gmail.com>
2018-04-24 10:59:25 -0300

    add missing stuff

--- include/dropboxClient.h
+++ include/dropboxClient.h
@@ -8,4 +8,18 @@ void get_file(char *file);
 void delete_file(char *file);
 void close_session ();
 
+typedef struct file_info {
+  char name[MAXNAME];
+  char extension[MAXNAME];
+  char last_modified[MAXNAME];
+  int size;
+} file_info_t;
+
+typedef struct client {
+  int devices[2];
+  char userid[MAXNAME];
+  struct file_info[MAXFILES];
+  int logged_in;
+} client_t;
+
 #endif
---
+++ testes/main.c
@@ -0,0 +1,6 @@
+#include <dropboxClient.h>
+
+int main(int argc, char const *argv[]) {
+  sync_client();
+  return 0;
+}
